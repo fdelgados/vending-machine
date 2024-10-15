62b7123fc57301c70640cb475db6124fb7e3df85
Cisco Delgado <fdelgados@gmail.com>
2024-10-15 15:48:44 +0200

    Change maintenance services

--- app/maintenance/src/Application/Restock/RestockService.php
+++ app/maintenance/src/Application/Restock/RestockService.php
@@ -3,20 +3,13 @@
 namespace VendingMachine\Maintenance\Application\Restock;
 
 use VendingMachine\Common\Domain\ProductId;
-use VendingMachine\Maintenance\Domain\ProductStock;
 use VendingMachine\Maintenance\Domain\Quantity;
 
 final readonly class RestockService
 {
-    public function __construct(private ProductStock $productStock)
-    {
-    }
-
     public function restock(RestockCommand $command): void
     {
         $productId = new ProductId($command->getProductId());
         $quantity = new Quantity($command->getQuantity());
-
-        $this->productStock->add($productId, $quantity);
     }
 }
